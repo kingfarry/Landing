91bca2f9a54072b84ddf0311c684c2d305de8d69
kingfarry <kingfarry57@gmail.com>
2024-04-03 12:15:38 -1200

    initial commit

---
+++ codeguru/index.js
@@ -0,0 +1,36 @@
+function checkout(){
+    // alert('Welcome to checkout')
+    // prompt('How old are you?')
+    confirm('Are you sure you want to pay?')
+}
+
+let age = 50
+age =   30
+console.log(age);
+
+const name = 'ade'
+// name = 'mary'
+console.log(name);
+
+// boolean
+const isStudent = true
+console.log(isStudent);
+
+// null
+const money = null
+console.log(money);
+
+// undefined
+let data;
+console.log(data);
+
+// Arrays
+let colours = ['white', 'blue', 'red', 'violet']
+console.log(colours);
+
+console.log(colours[1])
+
+const properties = [
+    'cars',
+    'houses'
+]
